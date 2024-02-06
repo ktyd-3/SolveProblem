b342f82fe1768dc7fc4f5009aae60344b0516f8b
ktyd-3 <teso26macr2@gmail.com>
2024-02-06 11:56:26 +0900

    webpack

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,26 +1,5 @@
-const { environment } = require("@rails/webpacker");
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-// 追加
-// ここから
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false,
-    },
-  },
-};
+const environment = require('./environment')
 
-environment.config.delete("node.dgram");
-environment.config.delete("node.fs");
-environment.config.delete("node.net");
-environment.config.delete("node.tls");
-environment.config.delete("node.child_process");
-
-environment.config.merge(customConfig);
-// ここまで
-
-module.exports = environment;
+module.exports = environment.toWebpackConfig()
