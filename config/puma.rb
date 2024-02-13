e994ed31810363fb19b127408f79299e42f8e215
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-02-13 11:21:57 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+
+#追加
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
