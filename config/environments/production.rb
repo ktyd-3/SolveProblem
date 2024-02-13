1fa381a946f6aa69b4144511801cff29b05f7afd
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-02-13 11:09:31 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -94,4 +95,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
 end
