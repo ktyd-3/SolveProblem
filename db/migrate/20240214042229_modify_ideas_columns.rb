dd0750c1395845e892babbb738382b8c6f4ee892
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-02-15 17:18:31 +0900

    pointカラムのデフォルト値調整

--- db/migrate/20240214042229_modify_ideas_columns.rb
+++ db/migrate/20240214042229_modify_ideas_columns.rb
@@ -1,6 +1,6 @@
 class ModifyIdeasColumns < ActiveRecord::Migration[7.1]
   def change
-    change_column_default :ideas, :easy_point, nil
-    change_column_default :ideas, :effect_point, nil
+    change_column_default :ideas, :easy_point, from: nil, to: 0
+    change_column_default :ideas, :effect_point, from: nil, to: 0
   end
 end
