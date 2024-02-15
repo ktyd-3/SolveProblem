19ac9febcd7f6a05572bf6c2d2afc78a83136889
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-02-15 17:28:38 +0900

    Delete 20240214042229_modify_ideas_columns.rb

--- db/migrate/20240214042229_modify_ideas_columns.rb
+++
@@ -1,6 +0,0 @@
-class ModifyIdeasColumns < ActiveRecord::Migration[7.1]
-  def change
-    change_column_default :ideas, :easy_point, from: nil, to: 0
-    change_column_default :ideas, :effect_point, from: nil, to: 0
-  end
-end
