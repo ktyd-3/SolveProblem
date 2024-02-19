97f91245e644354d030371bd393cd0b892335a1c
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-02-19 15:00:00 +0900

    webpack.config修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,5 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
-./bin/rails db:migrate
-
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
