6fc6202ae7a9b647e14fca04771bcf2737cd956f
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-03-14 11:24:10 +0900

    ideatree削除の削除turbo化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,9 @@ module ApplicationHelper
     list = ""
     if idea.children.any?
       idea.children.each do |child|
+        list += "<div id=\"list_#{child.id}\">"
         list += "&nbsp;" * (child.tree_level.to_i * 2) + "・" + link_to(child.name, solution_idea_path(child), data: { turbo_frame: "_top" }) + "<br>"
+        list += "</div>"
         list += display_descendants(child)
       end
     end
