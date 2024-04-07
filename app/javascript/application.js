1fabe908183c71388e2bfcb37c6b3b56be438d5e
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-04-07 11:12:32 +0900

    JSのコードをまとめた

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,17 +5,9 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "chartkick";
 import "Chart.bundle";
-import "jquery";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-free";
 library.add(fas, far, fab);
-import jquery from "jquery";
-window.$ = jquery;
-
-document.addEventListener("turbo:load", function () {
-  var clipboard = new Clipboard(".clipboard-btn");
-  console.log(clipboard);
-});
