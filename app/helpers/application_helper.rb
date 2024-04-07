3a7e6112dd884007b3145cd06f680e467e35430a
ktyd-3 <150873817+ktyd-3@users.noreply.github.com>
2024-04-07 12:09:41 +0900

    アイデアツリーのデザイン修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     list = ""
     if idea.children.any?
       idea.children.each do |child|
-        list += "<div id=\"list_#{child.id}\">"
+        list += "<div class='tree_list_line' id=\"list_#{child.id}\">"
         list += "&nbsp;" * (child.tree_level.to_i * 2) + "・" + link_to(child.name, solution_idea_path(child), data: { turbo_frame: "_top" }) + "<br>"
         list += "</div>"
         list += display_descendants(child)
